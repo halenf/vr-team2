1d85b8b96c8d068064f0a04d8392d723b84859bc
Jackson Lucas <s210201@students.aie.edu.au>
2024-06-17 16:49:24 +0930

    Small amounts of polish
    Just changes to public vars to make the experience more smooth.
    Ive been working in private scenes so ill need to upload those at some point.

--- Assets/Scripts/Player/RodController.cs
+++ Assets/Scripts/Player/RodController.cs
@@ -12,17 +12,20 @@ public class RodController : MonoBehaviour
     [SerializeField] private float m_forceScale;
     public Vector3 setHandVelocity { set { m_handVelocity = value; } }
     [SerializeField] private GameObject m_hook;
+    [SerializeField] private Transform m_hookHold;
 
     // Update is called once per frame
     void Update()
     {
         if(m_lineLockPhase == 1)
         {
-            m_hook.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            m_hook.transform.position = Vector3.Slerp(m_hook.transform.position, m_hookHold.position, 0.5f);
+            m_hook.GetComponent<Rigidbody>().isKinematic = true;
         }
         if (m_lineLockPhase == 2)
         {
             //cast rod out
+            m_hook.GetComponent<Rigidbody>().isKinematic = false;
             m_hook.GetComponent<Rigidbody>().AddForce(m_handVelocity * m_forceScale);
             m_lineLockPhase = 0;
         }
