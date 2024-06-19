3fde5dc119058260584ce0c00b7c490794182835
Jackson Lucas <126209142+SkeleKing042@users.noreply.github.com>
2024-06-19 16:15:54 +0930

    Updated the errors (#18)
    * Changed ref name in reel fish
    
    m_hookedFish.data.fishModel changed to m_hookedFish.data.model
    
    * Update reel fish again
    
    * Update reel fish once more

--- Assets/Scripts/FishingRod/ReelFish.cs
+++ Assets/Scripts/FishingRod/ReelFish.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -9,25 +10,33 @@ namespace FishingGame
     {
         public class ReelFish : MonoBehaviour
         {
+            private Transform m_player;
+            private Transform m_bobber;
+            [SerializeField] private float m_catchRadius;
             private Fish m_hookedFish;
-            public Fish hookFish { get { return m_hookedFish; } set { m_hookedFish = value; } }
+            public Fish SetHookedFish { set { m_hookedFish = value; } }
             [SerializeField] private Transform m_fishDisplayPoint;
-            [SerializeField] private float m_reelSpeed = 1;
+            
+            private void Start()
+            {
+                m_player = GameObject.FindGameObjectWithTag("Player").transform;
+                m_bobber = GameObject.FindGameObjectWithTag("Bobber").transform;
+            }
             /// <summary>
             /// Upon getting near to the player/pier, pull the fish from the water.
             /// </summary>
             public int SurfaceFish()
             {
-                if (m_hookedFish)
+                if (Vector3.Distance(m_player.position, m_bobber.position) < m_catchRadius)
                 {
                     //Instance the fish model as a child of the empty parent
-                    GameObject caughtFish = Instantiate(m_hookedFish.data.fishModel, m_fishDisplayPoint);
                     GameObject caughtFish = Instantiate(m_hookedFish.data.model, m_fishDisplayPoint);
                     //Its a grabbale kinematic rigidbody, so add the components
                     Rigidbody fishRb = caughtFish.AddComponent<Rigidbody>();
                     XRGrabInteractable fishGrab = caughtFish.AddComponent<XRGrabInteractable>();
                     fishRb.isKinematic = true;
                     fishGrab.useDynamicAttach = true;
+                    FindObjectOfType<RodController>().MountBobber();
                     return 0;
                 }
                 return -1;
