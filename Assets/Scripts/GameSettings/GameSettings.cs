18a5a68282cf7e1b896bfdf3fcc0aeb6669941a8
halenf <s210172@students.aie.edu.au>
2024-06-19 15:47:53 +0930

    Changed Pool
    Pool is now Round.

--- Assets/Scripts/GameSettings/GameSettings.cs
+++ Assets/Scripts/GameSettings/GameSettings.cs
@@ -7,15 +7,27 @@ namespace FishingGame
     public static class GameSettings
     {
         public static Vector3 POOL_ORIGIN;
-        public static Vector2 POOL_BOUNDS;
+        public static float POOL_RADIUS;
         public static int MAX_FISH_COUNT;
 
         public static float POOL_HEIGHT { get { return POOL_ORIGIN.y; } }
+        
+        public static Vector3 get_random_position_in_pool()
+        {
+            float radius = Random.Range(0, POOL_RADIUS);
+            float angle = Random.Range(0, 2f * Mathf.PI);
+
+            Vector3 position = new Vector3(POOL_ORIGIN.x + radius * Mathf.Cos(angle),
+                                           POOL_ORIGIN.y,
+                                           POOL_ORIGIN.z + radius * Mathf.Sin(angle));
+
+            return position;
+        }
 
         public static void LoadSettingsFromAsset(GameSettingsAsset asset)
         {
             POOL_ORIGIN = asset.poolOrigin;
-            POOL_BOUNDS = asset.poolBounds;
+            POOL_RADIUS = asset.poolRadius;
             MAX_FISH_COUNT = asset.maxFishCount;
         }
     }
