0b8e5ef4ada4e07904034ca77361924d6315c632
Jackson Lucas <s210201@students.aie.edu.au>
2024-06-24 16:54:55 +0930

    Update Bobber.cs
    Got a framework setup

--- Assets/Scripts/FishingRod/Bobber/Bobber.cs
+++ Assets/Scripts/FishingRod/Bobber/Bobber.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FishingGame.Objects;
+using FishingGame.AI;
 
 namespace FishingGame
 {
@@ -10,16 +12,41 @@ namespace FishingGame
 
         public class Bobber : MonoBehaviour
         {
+            private BuoyantObject m_datBuoy;
+            public bool isUnderwater { get { return m_datBuoy.isUnderwater; } }
+            private Agent m_hookedAgent;
+            public bool hasHookedAgent { get { return m_hookedAgent != null; } }
+
             // Start is called before the first frame update
             void Start()
             {
-
+                m_datBuoy = GetComponent<BuoyantObject>();
             }
 
             // Update is called once per frame
             void Update()
             {
+                
+            }
 
+            public void ReelIn()
+            {
+                Vector3 delta = Vector3.zero;
+
+                if (hasHookedAgent)
+                {
+                    //delta += m_hookedAgent.pullStrength;
+                }
+
+                //delta += rod.reelStrength;
+
+                transform.position += delta;
+            }
+
+            public void HookFish(Agent agent)
+            {
+                m_hookedAgent = agent;
+                m_hookedAgent.transform.SetParent(transform, false);
             }
         }
     }
