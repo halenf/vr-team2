ea238340634df3143e98b35021cc754f4c3f14dd
halenf <s210172@students.aie.edu.au>
2024-06-24 16:39:28 +0930

    Fix/Removal
    Added the new Fish Particle to the Fish prefab.
    Removed the Shadow section from the FishAnimationController.
    Removed ExportTerrain script.

--- Assets/Scripts/Fish/FishAnimationController.cs
+++ Assets/Scripts/Fish/FishAnimationController.cs
@@ -9,16 +9,8 @@ namespace FishingGame
     {
         public class FishAnimationController : MonoBehaviour
         {
-            [Header("Shadow")]
-            [SerializeField] private ParticleSystem m_shadowSystem;
-            [SerializeField] private float m_minShadowtime;
-            [SerializeField] private float m_maxShadowTime;
-
             [Header("Bubbles")]
             [SerializeField] private ParticleSystem m_bubbleSystem;
-            public ParticleSystem bubbleSystem { get { return m_bubbleSystem; } }
-            [SerializeField] private float m_minBubbleTime;
-            [SerializeField] private float m_maxBubbleTime;
 
             private void Update()
             {
