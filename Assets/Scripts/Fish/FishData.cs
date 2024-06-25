47a85a52b5b9f698c65a29b9e3637f68ff1db5b2
halenf <s210172@students.aie.edu.au>
2024-06-25 16:08:49 +0930

    Spooked/Buoy
    Created Spooked Behaviour and State.
    No States currently Transition to Spooked (needs Bobber justLanded check).
    Edited BuoyantObject script to be simpler.
    Added ToolTips to FishData.
    Removed playerDetectionRange from FishData (deprecated).

--- Assets/Scripts/Fish/FishData.cs
+++ Assets/Scripts/Fish/FishData.cs
@@ -32,21 +32,21 @@ namespace FishingGame
         [Header("Core Data")]
         [SerializeField] private string m_speciesName; // key
         [Space()]
-        [SerializeField] private Constraint m_weight;
-        [SerializeField] private Constraint m_length;
+        [SerializeField, Tooltip("Kilograms")] private Constraint m_weight;
+        [SerializeField, Tooltip("Metres")] private Constraint m_length;
 
         [Header("AI Data")]
-        [SerializeField] private Constraint m_lifeTime;
-        [SerializeField] private Constraint m_swimSpeed;
-        [SerializeField] private Constraint m_chaseSpeed;
-        [SerializeField] private Constraint m_spookSpeed;
-        [SerializeField] private Constraint m_swimRange;
-        [SerializeField] private Constraint m_swimDetectionRange;
-        [SerializeField] private Constraint m_bobberDetectionRange;
-        [SerializeField] private Constraint m_playerDetectionRange;
-        [SerializeField] private Constraint m_swimWaitTime;
-        [SerializeField] private Constraint m_bobberWaitTime;
-        [SerializeField] private Constraint m_pullStrength;
+        [SerializeField, Tooltip("Seconds")] private Constraint m_lifeTime;
+        [SerializeField, Tooltip("Units per second")] private Constraint m_swimSpeed;
+        [SerializeField, Tooltip("Units per second")] private Constraint m_chaseSpeed;
+        [SerializeField, Tooltip("Units per second")] private Constraint m_spookSpeed;
+        [SerializeField, Tooltip("Units")] private Constraint m_swimRange;
+        [SerializeField, Tooltip("Units")] private Constraint m_spookRange;
+        [SerializeField, Tooltip("Units")] private Constraint m_swimDetectionRange;
+        [SerializeField, Tooltip("Units")] private Constraint m_bobberDetectionRange;
+        [SerializeField, Tooltip("Seconds")] private Constraint m_swimWaitTime;
+        [SerializeField, Tooltip("Seconds")] private Constraint m_bobberWaitTime;
+        [SerializeField, Tooltip("Multiplier")] private Constraint m_pullStrength;
 
         // object
         public GameObject model { get { return m_model; } }
@@ -63,9 +63,9 @@ namespace FishingGame
         public Constraint chaseSpeed { get { return m_chaseSpeed; } }
         public Constraint spookSpeed { get { return m_spookSpeed; } }
         public Constraint swimRange { get { return m_swimRange; } }
+        public Constraint spookRange { get { return m_spookRange; } }
         public Constraint swimDetectionRange { get { return m_swimDetectionRange; } }
         public Constraint bobberDetectionRange { get { return m_bobberDetectionRange; } }
-        public Constraint playerDetectionRange { get { return m_playerDetectionRange; } }
         public Constraint swimWaitTime { get { return m_swimWaitTime; } }
         public Constraint bobberWaitTime { get { return m_bobberWaitTime; } }
         public Constraint pullStrength { get { return m_pullStrength; } }
